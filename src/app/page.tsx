53eb1036e371bcd59465d087ca45b333ab90a3f8
waleedprogramer <waleedbinmehmoodahmed@gmail.com>
2024-11-03 18:10:44 +0500

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import About from "./about/page";
+import Contact from "./contact/page";
+import Main from "./home/page";
+import Services from "./services/page";
+
+export default function Home() {
+  return (
+    <>
+    <Main/>
+    <About/>
+    <Services/>
+    <Contact/>
+    </>
+  );
+}
